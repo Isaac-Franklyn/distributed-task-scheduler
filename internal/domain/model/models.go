23757dedbfc290c1af1a24c9c71e3b0c65cc26d5
Isaac-Franklyn <officialisaacfranklyn@gmail.com>
2025-04-03 08:45:19 +0530

    defined task and raft node structs in the models.go file

---
+++ internal/domain/model/models.go
@@ -0,0 +1,17 @@
+package model
+
+import "github.com/hashicorp/raft"
+
+type Task struct {
+	ID       string `json:"id"`
+	Payload  any    `json:"payload"`
+	Type     string `json:"type"`
+	Priority int    `json:"priority"`
+	Status   string `json:"status"`
+	Retries  int    `json:"retries"`
+}
+
+type Node struct {
+	ID   string
+	Raft *raft.Raft
+}
